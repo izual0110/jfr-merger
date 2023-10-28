eac48c018e4db8c454f0781f183bbf7073e0d27d
Maxim Tsoy <mtsoy1990@gmail.com>
2023-10-28 21:51:45 +0600

    fixed directories

--- cli/src/main/java/com/jfrmerger/cli/JfrMergerConsoleApplication.java
+++ cli/src/main/java/com/jfrmerger/cli/JfrMergerConsoleApplication.java
@@ -21,6 +21,6 @@ public class JfrMergerConsoleApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        log.info("Hello!");
+        log.info("Hello!!!");
     }
 }
