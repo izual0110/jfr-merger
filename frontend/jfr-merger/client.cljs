47c249303ca8e4adb396e7834d18343b09a0e527
Maxim Tsoy <mtsoy1990@gmail.com>
2025-01-12 00:04:28 +0900

    init

---
+++ frontend/jfr-merger/client.cljs
@@ -0,0 +1,11 @@
+(ns jfr-merger.client
+;;   (:require
+;;    [reagent.core :as reagent]
+;;    [reagent.dom :as rdom]
+;;    [cljsjs.react]
+;;    )
+  )
+
+(js/alert "test")
+
+(+1 2 3)
--- src/jfr_merger/core.clj
+++
@@ -1,7 +0,0 @@
-(ns jfr-merger.core
-  (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
