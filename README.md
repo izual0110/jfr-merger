014a012e094db1eeba6d2fc3a32cf64cec247865
Maxim Tsoy <mtsoy1990@gmail.com>
2025-01-26 14:31:09 +0900

    hotfix

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ FIXME: description
 > cd ...
 > docker run -it -v $(pwd):/app -w /app node:23 npm install react@17.0.2 react-dom@17.0.2 create-react-class
 > docker run -it -v $(pwd):/app -w /app node:23 npm install --save-dev shadow-cljs
+> mkdir -p lib && curl -o lib/jfr-converter.jar https://github.com/async-profiler/async-profiler/releases/download/nightly/jfr-converter.jar
+> clj -M:cljs
 
 {
     "dependencies": {
--- src/clj/jfr/core.clj
+++ src/clj/jfr/core.clj
@@ -3,6 +3,7 @@
         [compojure.core :only [defroutes GET POST DELETE ANY context]]
         [org.httpkit.server :refer [run-server]]
         [hiccup.core :refer [html]])
+  (:import [one.convert JfrToHeatmap])
   (:gen-class))
 
 (defn index [req]
@@ -10,8 +11,19 @@
    :headers {"Content-Type" "text/html"}
    :body    (html [:a {:href "/index.html"} "index"])})
 
+
+(System/getProperty "user.dir") 
+
+(JfrToHeatmap/convert "input" "output" nil)
+
+(defn generate-heatmap [body]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (html [:a {:href "/index.html"} (slurp (.bytes body))])})
+
 (defroutes app
   (GET "/" [] index)
+  (POST "/heatmap" {body :body} (generate-heatmap body))
   (resources "/"))
 
 (defonce server (atom nil))
