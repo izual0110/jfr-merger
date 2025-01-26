119e69c48a5d2fd048179fd4258e9b52b95c2f12
Maxim Tsoy <mtsoy1990@gmail.com>
2025-01-26 13:29:58 +0900

    updated form

--- src/cljs/jfr/client.cljs
+++ src/cljs/jfr/client.cljs
@@ -4,19 +4,17 @@
    [reagent.dom :as rdom]
    [react-dom :as re]))
 
-;; (js/alert (str "test " (+ 1 2 3 4)))
-
-;; (js/console.log "Hello World  124")
-
-;; (re/render)
-
-;; (defn ^:export init []
-  ;; (js/console.log "Hello World"))
 
 (defn app []
-      [:div "Hello World"])
-
-;; (js/console.log (js/document.getElementById "root"))
+      [:div {:class "container"} 
+       [:section {:class "section"}
+       [:div {:class "box"} 
+        [:p {:class "title"} "paste the link"]
+        [:div {:class "field has-addons"}
+         [:div {:class "control is-expanded"}
+          [:input {:type "text" :class "input"}]]
+         [:div {:class "control"}
+          [:button {:class "button"} "Submit"]]]]]])
 
 (defn ^:export init []
   (js/console.log "Hello World")
