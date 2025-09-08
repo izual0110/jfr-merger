2209b73db17628066bf5947df357de63f804939d
Maxim Tsoy <mtsoy1990@gmail.com>
2025-09-08 09:59:59 +0900

    hotfix

--- test/test_runner.clj
+++ test/test_runner.clj
@@ -8,4 +8,6 @@
         nss (ns-find/find-namespaces-in-dir (first dirs))]
     (println "Running tests in namespaces:" nss)
     (apply require nss)
-    (apply t/run-tests nss)))
+    (let [{:keys [fail error]} (apply t/run-tests nss)]
+      (shutdown-agents)
+      (System/exit (if (pos? (+ (or fail 0) (or error 0))) 1 0)))))
