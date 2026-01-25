867e62cf1bf5baa4deb8f73f579dedf8557af412
Maxim <1565682+izual0110@users.noreply.github.com>
2026-01-25 03:48:18 +0300

    Add server lifecycle test

--- test/jfr/core_test.clj
+++ test/jfr/core_test.clj
@@ -1,6 +1,9 @@
 (ns jfr.core-test
   (:require [clojure.test :refer :all]
-            [jfr.core :as core]))
+            [jfr.core :as core]
+            [jfr.detector.worker :as detector-worker]
+            [jfr.storage :as storage]
+            [org.httpkit.server :as http-kit]))
 
 (deftest index-html-served
   (let [response (core/app {:request-method :get
@@ -11,3 +14,37 @@
                (slurp body))]
     (is (= 200 (:status response)))
     (is (seq text))))
+
+(deftest start-stop-server
+  (let [original-server @core/server
+        init-called (atom false)
+        start-called (atom false)
+        stop-worker-called (atom false)
+        destroy-called (atom false)
+        stop-args (atom nil)
+        run-args (atom nil)]
+    (with-redefs [http-kit/run-server (fn [handler opts]
+                                        (reset! run-args {:handler handler
+                                                          :opts opts})
+                                        (fn [& args]
+                                          (reset! stop-args args)))
+                  storage/init (fn [] (reset! init-called true))
+                  storage/destroy (fn [] (reset! destroy-called true))
+                  detector-worker/start! (fn [] (reset! start-called true))
+                  detector-worker/stop! (fn [] (reset! stop-worker-called true))]
+      (try
+        (core/-main)
+        (is @init-called)
+        (is @start-called)
+        (is (fn? @core/server))
+        (let [handler (:handler @run-args)
+              response (handler {:request-method :get
+                                 :uri "/index.html"})]
+          (is (= 200 (:status response))))
+        (core/stop-server)
+        (is @stop-worker-called)
+        (is @destroy-called)
+        (is (= [:timeout 100] @stop-args))
+        (is (nil? @core/server))
+        (finally
+          (reset! core/server original-server))))))
