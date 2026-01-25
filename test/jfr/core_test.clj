d2432f53d6630fd0f938aed9fe4f230103c92055
Maxim <1565682+izual0110@users.noreply.github.com>
2026-01-25 15:57:07 +0300

    fix server port for test and prod (#68)

--- test/jfr/core_test.clj
+++ test/jfr/core_test.clj
@@ -1,5 +1,8 @@
 (ns jfr.core-test
+  (:import [java.util UUID]
+           [java.io File])
   (:require [clojure.test :refer :all]
+            [jfr.environ :as env]
             [jfr.core :as core]
             [org.httpkit.client :as http-client]))
 
@@ -14,31 +17,33 @@
     (is (seq text))))
 
 (deftest start-stop-server
-  (let [original-server @core/server]
-    (try
-      (core/-main)
-      (is (fn? @core/server))
-      (loop [attempts 10]
-        (let [response (try
-                         @(http-client/get "http://localhost:8080/index.html"
-                                           {:timeout 2000})
-                         (catch Exception _ nil))]
-          (cond
-            (= 200 (:status response))
-            (do
-              (is (seq (:body response)))
-              (is true))
+  (with-redefs [env/get-jfr-data-path (fn [] (.getAbsolutePath (File. (str "target/jfr-test-db-" (UUID/randomUUID)))))]
+    (let [original-server @core/server
+          port 8181]
+      (try
+        (core/start-server port)
+        (is (fn? @core/server))
+        (loop [attempts 10]
+          (let [response (try
+                           @(http-client/get (str "http://localhost:" port "/index.html")
+                                             {:timeout 2000})
+                           (catch Exception _ nil))]
+            (cond
+              (= 200 (:status response))
+              (do
+                (is (seq (:body response)))
+                (is true))
 
-            (zero? attempts)
-            (is false (str "Unexpected status: " (:status response)))
+              (zero? attempts)
+              (is false (str "Unexpected status: " (:status response)))
 
-            :else
-            (do
-              (Thread/sleep 100)
-              (recur (dec attempts))))))
-      (catch Exception e
-        (is false (str "Unexpected exception: " e)))
-      (finally
-        (core/stop-server)
-        (is (nil? @core/server))
-        (reset! core/server original-server)))))
+              :else
+              (do
+                (Thread/sleep 100)
+                (recur (dec attempts))))))
+        (catch Exception e
+          (is false (str "Unexpected exception: " e)))
+        (finally
+          (core/stop-server)
+          (is (nil? @core/server))
+          (reset! core/server original-server))))))
