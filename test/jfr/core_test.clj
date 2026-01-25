c0409299ae4a8e52abdf4783aeaa4dc0e124debc
Maxim <1565682+izual0110@users.noreply.github.com>
2026-01-25 05:04:27 +0300

    Update server lifecycle test to call `core/-main` and validate `index.html` response (#64)
    * Change max-body limit to Integer/MAX_VALUE

--- test/jfr/core_test.clj
+++ test/jfr/core_test.clj
@@ -1,6 +1,7 @@
 (ns jfr.core-test
   (:require [clojure.test :refer :all]
-            [jfr.core :as core]))
+            [jfr.core :as core]
+            [org.httpkit.client :as http-client]))
 
 (deftest index-html-served
   (let [response (core/app {:request-method :get
@@ -11,3 +12,33 @@
                (slurp body))]
     (is (= 200 (:status response)))
     (is (seq text))))
+
+(deftest start-stop-server
+  (let [original-server @core/server]
+    (try
+      (core/-main)
+      (is (fn? @core/server))
+      (loop [attempts 10]
+        (let [response (try
+                         @(http-client/get "http://localhost:8080/index.html"
+                                           {:timeout 2000})
+                         (catch Exception _ nil))]
+          (cond
+            (= 200 (:status response))
+            (do
+              (is (seq (:body response)))
+              (is true))
+
+            (zero? attempts)
+            (is false (str "Unexpected status: " (:status response)))
+
+            :else
+            (do
+              (Thread/sleep 100)
+              (recur (dec attempts))))))
+      (catch Exception e
+        (is false (str "Unexpected exception: " e)))
+      (finally
+        (core/stop-server)
+        (is (nil? @core/server))
+        (reset! core/server original-server)))))
