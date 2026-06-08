942bdfe4f22c4c222435f2c148dee4e084e73f9e
Maxim <1565682+izual0110@users.noreply.github.com>
2026-06-08 15:50:12 +0900

    Replace UI with simple path processing page

--- test/jfr/heapdump_test.clj
+++ test/jfr/heapdump_test.clj
@@ -22,10 +22,20 @@
                  (.getAbsolutePath))]
     (try
       (.dumpHeap (hotspot-diagnostic) path true)
-      (let [output (heapdump/heapdump-stats-text path)]
+      (let [output (heapdump/heapdump-stats-text path)
+            path-output (heapdump/handle-heapdump-upload {:params {"path" path}})]
         (is (string? output))
         (is (.contains output "Heap Dump:"))
-        (is (.contains output "=== Class Histogram")))
+        (is (.contains output "=== Class Histogram"))
+        (is (string? path-output))
+        (is (.contains path-output "Heap Dump:"))
+        (is (.contains path-output "=== Class Histogram")))
       (finally
         (io/delete-file path true)
         (io/delete-file (.toFile temp-dir) true)))))
+
+
+(deftest handle-heapdump-path-requires-readable-file
+  (is (thrown-with-msg? IllegalArgumentException
+                        #"readable file"
+                        (heapdump/handle-heapdump-upload {:params {"path" "/tmp/definitely-missing-heapdump.hprof"}}))))
